90d64f98accb18b56ec0c3ba289ac0d580858630
Lee Farrugia <farrugialee@gmail.com>
2023-10-18 09:59:18 +0200

    Final Trial run

--- Pylinac/Pylinac_Demo.py
+++ Pylinac/Pylinac_Demo.py
@@ -6,3 +6,12 @@ ct504 = CatPhan504(f)
 
 ct504.analyze()
 print(ct504.results())
+
+ct504.save_analyzed_image('ct504_analyzed.png')
+
+ct504.save_analyzed_subimage('ct504_rmtf', 'rmtf')
+ct504.save_analyzed_subimage('ct504_hu', 'hu')
+ct504.save_analyzed_subimage('ct504_lin', 'lin')
+
+ct504.plot_analyzed_subimage()
+
